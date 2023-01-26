3469e546108718d6e0b68a2098d307ef9cf0a2ee
owais11-art <owaiszahoor79@gmail.com>
2023-01-26 15:22:54 +0530

    changed image path

--- src/data/site-data.js
+++ src/data/site-data.js
@@ -3,7 +3,7 @@ const menuItems = [
         id: 1,
         name: 'French Fries with Ketchup',
         price: 2.23,
-        image: 'plate__french-fries.png',
+        image: '/src/assets/plate__french-fries.png',
         alt: 'French Fries',
         count: 0
     },
@@ -11,7 +11,7 @@ const menuItems = [
         id: 2,
         name: 'Salmon and Vegetables',
         price: 5.12,
-        image: 'plate__salmon-vegetables.png',
+        image: '/src/assets/plate__salmon-vegetables.png',
         alt: 'Salmon and Vegetables',
         count: 0
     },
@@ -19,7 +19,7 @@ const menuItems = [
         id: 3,
         name: 'Spaghetti Meat Sauce',
         price: 7.82,
-        image: 'plate__spaghetti-meat-sauce.png',
+        image: '/src/assets/plate__spaghetti-meat-sauce.png',
         alt: 'Spaghetti with Meat Sauce',
         count: 0
     },
@@ -27,7 +27,7 @@ const menuItems = [
         id: 4,
         name: 'Bacon, Eggs, and Toast',
         price: 5.99,
-        image: 'plate__bacon-eggs.png',
+        image: '/src/assets/plate__bacon-eggs.png',
         alt: 'Bacon, Eggs, and Toast',
         count: 0
     },
@@ -35,7 +35,7 @@ const menuItems = [
         id: 5,
         name: 'Chicken Salad with Parmesan',
         price: 6.98,
-        image: 'plate__chicken-salad.png',
+        image: '/src/assets/plate__chicken-salad.png',
         alt: 'Chicken Salad with Parmesan',
         count: 0
     },
@@ -43,10 +43,10 @@ const menuItems = [
         id: 6,
         name: 'Fish Sticks and Fries',
         price: 6.34,
-        image: 'plate__fish-sticks-fries.png',
+        image: '/src/assets/plate__fish-sticks-fries.png',
         alt: 'Fish Sticks and Fries',
         count: 0
     }
-].map(item => ({...item, image:`/src/assets/${item.image}`, total: item.price * item.count}))
+].map(item => ({...item, total: item.price * item.count}))
 
 export default menuItems
